e17c3b7ac4e3b2cedb69891471b6b693353cf8de
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-07 15:12:43 +0900

    webpackの設定

---
+++ frontend/webpack.common.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const src = path.resolve(__dirname, "src");
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  entry: ["@babel/polyfill", src + "/index.jsx"],
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "eslint-loader",
+            options: {
+              formatter: require("eslint/lib/cli-engine/formatters/stylish")
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + "/public/index.html",
+      filename: "index.html"
+    })
+  ]
+};
