93087385e2a098a072a5bcd85c9723e0c17d13c1
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-07 16:15:46 +0900

    グローバルスタイルを適用

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 import Home from "./App/Home";
 
 class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <>
+        <GlobalStyle />
+        <Home />
+      </>
+    );
   }
 }
+
+const GlobalStyle = createGlobalStyle`
+  @import url(http://fonts.googleapis.com/earlyaccess/notosansjp.css);
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans JP', sans-serif;
+  }
+`;
+
 render(<App />, document.getElementById("root"));
