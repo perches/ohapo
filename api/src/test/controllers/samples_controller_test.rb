58e738d7778b5d9f214844d1f59816fc486d661d
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-09-09 01:32:52 +0900

    RSpecでテストするため不要なapp/src/testディレクトリを削除

--- api/src/test/controllers/samples_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SamplesControllerTest < ActionDispatch::IntegrationTest
-  test "should get healthcheck" do
-    get samples_healthcheck_url
-    assert_response :success
-  end
-
-end
