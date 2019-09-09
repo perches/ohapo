4271e1520eb7101ad9b3e947fcf21653c55552df
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-09-10 00:28:25 +0900

    docker-compose up -d実行時に、DBとテーブルが自動作成され、 ダミーデータが自動登録されることを確認

---
+++ etc/docker/db/init/2_data.sh
@@ -0,0 +1,1 @@
+mysql --defaults-extra-file=/docker-entrypoint-initdb.d/dbaccess.cnf --local-infile sada -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data.csv' INTO TABLE masashi FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
