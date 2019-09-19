12cb3cb7ce57b5eaf909ba76754d7140e4b74f7e
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-09-19 15:20:44 +0900

    dbコンテナ起動を待って、apiコンテナが起動するように制御

--- etc/docker/api/entrypoint.sh
+++ etc/docker/api/entrypoint.sh
@@ -3,6 +3,17 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# dbヘルスチェック
+echo `date '+%Y/%m/%d %H:%M:%S'` $0 "[INFO] Connection confriming..."
+while :
+do
+  result=`/usr/bin/mysqladmin ping -h db -uroot -p${MYSQL_ROOT_PASSWORD}`
+  if echo $result|grep 'alive'; then
+    break
+  fi
+  sleep 3;
+done
+
 bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
 
 exec "$@"
