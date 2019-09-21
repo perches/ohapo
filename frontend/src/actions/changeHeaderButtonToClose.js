d824b1106bef80330a7f8534d2c32e13bef3f969
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-21 20:05:58 +0900

    メニューをすべて閉じるためのactions作成

---
+++ frontend/src/actions/changeHeaderButtonToClose.js
@@ -0,0 +1,7 @@
+export const CHANGE_HEADER_BUTTON_TO_CLOSE = "CHANGE_HEADER_BUTTON_TO_CLOSE";
+
+const changeHeaderButtonToClose = () => ({
+  type: CHANGE_HEADER_BUTTON_TO_CLOSE
+});
+
+export default changeHeaderButtonToClose;
