e9aec69162e6f35e1e25679170403e1a9cf678da
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-21 23:21:04 +0900

    Loginコンポーネント追加

---
+++ frontend/src/App/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        this is login.
+      </>
+    );
+  }
+}
+
+export default Login;
