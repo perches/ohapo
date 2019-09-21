9edd72a0acd0865fb81a23528fc20aa196074fc4
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-21 23:17:07 +0900

    routing定義

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,10 +2,30 @@ import Home from "./App/Home";
 
 const routes = [
   {
-    path: "/Home",
+    path: "/home",
     component: Home,
     name: "ホーム"
   },
+  {
+    path: "/login",
+    component: Login,
+    name: "ログイン"
+  },
+  {
+    path: "user",
+    component: User,
+    name: "プロフィール"
+  },
+  {
+    path: "user/edit",
+    component: UserEdit,
+    name: "プロフィール編集"
+  },
+  {
+    path: "user/edit/confirm",
+    component: UserEditConfirm,
+    name: "プロフィール編集"
+  }
 ];
 
 export default routes;
