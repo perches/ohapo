3c9a310a946bb3d8691d59ff583f4a32d4babaee
hiro61922 <shaka61922@gmail.com>
2019-09-22 02:18:11 +0900

    静的解析コメント対応

--- api/src/db/migrate/20190921121030_create_user_news_categories.rb
+++ api/src/db/migrate/20190921121030_create_user_news_categories.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUserNewsCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :user_news_categories do |t|
--- api/src/db/migrate/20190921125519_remove_first_name_from_users.rb
+++ api/src/db/migrate/20190921125519_remove_first_name_from_users.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class RemoveFirstNameFromUsers < ActiveRecord::Migration[6.0]
   def change
-
     remove_column :users, :first_name, :string
   end
 end
