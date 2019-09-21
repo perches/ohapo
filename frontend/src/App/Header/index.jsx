2a14e9adcdcf22aaae0eec13bc4e3fcf3c22078e
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-21 17:47:44 +0900

    Headerファイル作成

---
+++ frontend/src/App/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  AppBar,
+} from "@material-ui/core";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <AppBar>
+        appbar
+      </AppBar>
+    )
+  }
+}
+
+export default Header;
