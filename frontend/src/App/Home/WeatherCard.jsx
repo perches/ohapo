7e83fecbf3c36715f43acb00b087046f629b8d07
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-23 04:02:35 +0900

    今日の天気をカード形式で表示する

--- frontend/src/App/Home/WeatherCard.jsx
+++ frontend/src/App/Home/WeatherCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { CardContent, Card } from "@material-ui/core";
+import styled from "styled-components";
+import { rgba } from "polished";
+import { CardContent, Card, Grid } from "@material-ui/core";
 import Moment from "react-moment";
 import "moment-timezone";
+import { theme } from "../../consts/theme";
 
 class Home extends React.Component {
   render() {
@@ -10,24 +13,18 @@ class Home extends React.Component {
       result
     } = this.props;
 
-    const this24Hours = {
-      weathers: [],
-      timeStamps: [],
-    };
+    const this24Hours = [];
     const city = result && result.city.name;
     result && buildWeather();
-    // const timeStamp = result && buildWeather();
-
-
     function buildWeather() {
       for(let i=0, n=(24/3); i<n; i++ ) {
-        // let dateTime = new Date([result.list[i].dt_txt]);
-
-        this24Hours.weathers.push([ result.list[i].weather[0].description]);
-        this24Hours.timeStamps.push(
-          // dateTime.setTime(dateTime.getTime() + 1000 * 60 * 60 * 9)
-          [result.list[i].dt_txt]
-        );
+        this24Hours[i] = { weather: '', timeStamp: '', iconUrl: '' };
+        this24Hours[i].weather = result.list[i].weather[0].description;
+        this24Hours[i].timeStamp =  (new Date([result.list[i].dt_txt])).getHours();
+        // TODO: ハードコード
+        this24Hours[i].iconUrl =  `http://openweathermap.org/img/wn/${[
+            result.list[i].weather[0].icon
+          ]}.png`;
       }
       return this24Hours;
     }
@@ -38,15 +35,23 @@ class Home extends React.Component {
 
     return (
       <Card>
-        <CardContent style={{ height: 180 }}>
-          <p>{city}</p>
-          <p>24時間の天気: {JSON.stringify(this24Hours)}</p>
-          <p>フォーマットされた時間:</p>
-          {this24Hours && this24Hours.timeStamps.map(time => (
-            <Moment date={time} />
-          ))}
-
-          {process.env.OPEN_WEATHER_API_URL}
+        <CardContent>
+          <Grid container justify="center">
+            <Grid item>
+              <p>{city}</p>
+            </Grid>
+          </Grid>
+          <Grid container justify="space-between">
+            {console.log(this24Hours)}
+            {this24Hours &&
+              this24Hours.map((w, index) => (
+                <EachDetailsContainer item key={index}>
+                  <WeatherText>{w.weather}</WeatherText>
+                  <img src={w.iconUrl} />
+                  <TimeStampText>{String(w.timeStamp)}時</TimeStampText>
+                </EachDetailsContainer>
+              ))}
+          </Grid>
         </CardContent>
       </Card>
     );
@@ -56,5 +61,24 @@ class Home extends React.Component {
 Home.propTypes = {
   result: PropTypes.object
 };
+
+const EachDetailsContainer = styled(Grid)`
+  border: 1px;
+  text-align: center;
+  background-color: ${rgba(theme.palette.primary.main, 0.2)};
+  border-radius: 8px;
+  width: calc((100% / 8) - 15px);
+  padding: 5px;
+`;
+
+const WeatherText = styled.p`
+  font-size: 14px;
+  color: ${theme.palette.muted.dark};
+`;
+
+const TimeStampText = styled.p`
+  font-size: 14px;
+  color: ${theme.palette.muted.dark};
+`;
 
 export default Home;
