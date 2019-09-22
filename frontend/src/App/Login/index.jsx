b66196a4a6189348be0cf32b9fac9907e9819fd5
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-22 15:41:21 +0900

    画像追加

--- frontend/src/App/Login/index.jsx
+++ frontend/src/App/Login/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import BackgroundImage from "../../public/assets/morning_coffee.png";
 
 class Login extends React.Component {
   render() {
     return (
       <>
-        <p>this is Login.</p>
+        <Wrapper>aa</Wrapper>
       </>
     );
   }
 }
+
+const Wrapper = styled.div`
+  font-size: 111px;
+  background-color: #333;
+  background-image: url("${BackgroundImage}");
+`;
 
 export default Login;
