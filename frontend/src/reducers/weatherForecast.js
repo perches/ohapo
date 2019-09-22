4c46fc19f8cb750fc9e5db6962745d579b350e97
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-22 21:36:04 +0900

    OpenWeatherのreducers

---
+++ frontend/src/reducers/weatherForecast.js
@@ -0,0 +1,33 @@
+import {
+  FETCH_WEATHER_FORECAST,
+  FETCH_WEATHER_FORECAST_SUCCESS,
+  FETCH_WEATHER_FORECAST_ERROR
+} from "../actions/fetchWeatherForecast";
+
+const initialState = {
+  result: null,
+  isLoading: false,
+  error: false
+};
+
+export default function weatherForecastReducer(state = initialState, action) {
+  switch (action.type) {
+  case FETCH_WEATHER_FORECAST:
+    return Object.assign({}, state, {
+      isLoading: true
+    });
+  case FETCH_WEATHER_FORECAST_SUCCESS:
+    return Object.assign({}, state, {
+      result: action.payload.weatherForecast.result,
+      isLoading: false,
+      error: false
+    });
+  case FETCH_WEATHER_FORECAST_ERROR:
+    return Object.assign({}, state, {
+      isLoading: false,
+      error: true
+    });
+  default:
+    return state;
+  }
+}
