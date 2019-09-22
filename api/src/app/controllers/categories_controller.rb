c5c892af6c1177366dd0abeefc7567214fa825b2
16motsu <masaya.0328.k@gmail.com>
2019-09-23 00:41:27 +0900

    CategoryControllerを追加

---
+++ api/src/app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def showall
+    json = {
+      categories: 
+      [
+        {
+          id: 1,
+          category_name: "business"
+        },
+        {
+          id: 2,
+          category_name: "entertainment"
+        },
+        {
+          id: 3,
+          category_name: "health"
+        },
+        {
+          id: 4,
+          category_name: "science"
+        },
+        {
+          id: 5,
+          category_name: "sports"
+        },
+        {
+          id: 6,
+          category_name: "technology"
+        },
+      ]
+    }
+    render json: json
+  end
+end
