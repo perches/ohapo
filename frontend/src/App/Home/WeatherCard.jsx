73cc1b8cf2a0f58a8dd2d3993b1380e5c32bd5c4
Yuto Tachibana <tachibanayu24@gmail.com>
2019-09-23 12:31:41 +0900

    気温などをグラフで表示

--- frontend/src/App/Home/WeatherCard.jsx
+++ frontend/src/App/Home/WeatherCard.jsx
@@ -1,9 +1,64 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 import { rgba } from "polished";
 import { CardContent, Card, Grid } from "@material-ui/core";
 import { theme } from "../../consts/theme";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
 
 class Home extends React.Component {
   render() {
@@ -12,19 +67,38 @@ class Home extends React.Component {
     } = this.props;
 
     const this24Hours = [];
+    const chartData = [];
     const city = result && result.city.name;
     result && buildWeather();
+    result && buildChartData();
     function buildWeather() {
       for(let i=0, n=(24/3); i<n; i++ ) {
         this24Hours[i] = { weather: '', timeStamp: '', iconUrl: '' };
+
         this24Hours[i].weather = result.list[i].weather[0].description;
-        this24Hours[i].timeStamp =  (new Date([result.list[i].dt_txt])).getHours();
+        this24Hours[i].timeStamp =  `${(new Date([result.list[i].dt_txt])).getHours()}時`;
         // TODO: ハードコード
         this24Hours[i].iconUrl =  `http://openweathermap.org/img/wn/${[
           result.list[i].weather[0].icon
         ]}.png`;
       }
+      console.log(result);
+      console.log(this24Hours);
       return this24Hours;
+    }
+
+    function buildChartData() {
+      for (let i = 0, n = 24 / 3; i < n; i++) {
+        chartData[i] = { name: "", temperature: "", rainyPercent: "" };
+
+        chartData[i].name = `${new Date([result.list[i].dt_txt]).getHours()}時`;
+        chartData[i].temperature =
+          result.list[i].main.temp - process.env.KELVIN;
+      }
+      console.log(process.env.KELVIN);
+      console.log(chartData);
+      console.log(data);
+      return chartData;
     }
 
 
@@ -49,13 +123,58 @@ class Home extends React.Component {
                 {this24Hours &&
                   this24Hours.map((w, index) => (
                     <EachDetailsContainer item key={index}>
-                      <WeatherText>{w.weather}</WeatherText>
+                      <TimeStampText>{String(w.timeStamp)}</TimeStampText>
                       <img src={w.iconUrl} />
-                      <TimeStampText>{String(w.timeStamp)}時</TimeStampText>
+                      <WeatherText>{w.weather}</WeatherText>
                     </EachDetailsContainer>
                   ))}
               </DetailsWrapperAbsolute>
             </DetailsWrapperRelative>
+            <Grid container justify="center">
+              {chartData && (
+                <ChartContainer style={{ width: "100%", height: 300 }}>
+                  <ResponsiveContainer>
+                    <LineChart
+                      width={500}
+                      height={300}
+                      data={chartData}
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+                      <YAxis yAxisId="left" />
+                      <YAxis yAxisId="right" orientation="right" />
+                      <Tooltip />
+                      <Legend />
+                      <Line
+                        yAxisId="left"
+                        type="monotone"
+                        dataKey="temperature"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                      />
+                      {/* <Line
+                      yAxisId="left"
+                      type="monotone"
+                      dataKey="uv"
+                      stroke="#82ca9d"
+                    />
+                    <Line
+                      yAxisId="right"
+                      type="monotone"
+                      dataKey="amt"
+                      stroke="#82ca9d"
+                    /> */}
+                    </LineChart>
+                  </ResponsiveContainer>
+                </ChartContainer>
+              )}
+            </Grid>
           </CardContent>
         </Wrapper>
       </Card>
@@ -69,7 +188,7 @@ Home.propTypes = {
 
 const Wrapper = styled.div`
   background-color: ${rgba(theme.palette.muted.main, 0.4)};
-  height: 200px;
+  height: 600px;
 `;
 
 const CityWrapperAbsolute = styled.div`
@@ -124,6 +243,11 @@ const WeatherText = styled.p`
 const TimeStampText = styled.p`
   font-size: 14px;
   color: ${theme.palette.muted.dark};
+`;
+
+const ChartContainer = styled.div`
+  -webkit-transform: translate(0%, 50%);
+  transform: translate(0%, 50%);
 `;
 
 export default Home;
