30a707b67ccdea3851ac8ee6e0c7f67a39650b20
16motsu <masaya.0328.k@gmail.com>
2019-09-23 13:05:55 +0900

    rspec修正

--- api/src/spec/requests/categories_controller_spec.rb
+++ api/src/spec/requests/categories_controller_spec.rb
@@ -3,10 +3,11 @@
 require "rails_helper"
 
 RSpec.describe CategoriesController, type: :controller do
+
   describe "GET /categories" do
     before do
       # 共通処理を記述
-      get "categories"
+      get :index
     end
 
     context "should return response" do
@@ -15,21 +16,21 @@ RSpec.describe CategoriesController, type: :controller do
       end
     end
 
-    context "should return string" do
+    context "should return JSON" do
       it "that return is Categories" do
         json = JSON.parse(response.body)
-        expect(json["categories"][1]["id"]).to eq(1)
-        expect(json["categories"][2]["id"]).to eq(2)
-        expect(json["categories"][3]["id"]).to eq(3)
-        expect(json["categories"][4]["id"]).to eq(4)
-        expect(json["categories"][5]["id"]).to eq(5)
-        expect(json["categories"][6]["id"]).to eq(6)
-        expect(json["categories"][1]["category_name"]).to eq("business")
+        expect(json["categories"][0]["id"]).to eq(1)
+        expect(json["categories"][1]["id"]).to eq(2)
+        expect(json["categories"][2]["id"]).to eq(3)
+        expect(json["categories"][3]["id"]).to eq(4)
+        expect(json["categories"][4]["id"]).to eq(5)
+        expect(json["categories"][5]["id"]).to eq(6)
+        expect(json["categories"][0]["category_name"]).to eq("business")
         expect(json["categories"][1]["category_name"]).to eq("entertainment")
-        expect(json["categories"][1]["category_name"]).to eq("health")
-        expect(json["categories"][1]["category_name"]).to eq("science")
-        expect(json["categories"][1]["category_name"]).to eq("sports")
-        expect(json["categories"][1]["category_name"]).to eq("technology")
+        expect(json["categories"][2]["category_name"]).to eq("health")
+        expect(json["categories"][3]["category_name"]).to eq("science")
+        expect(json["categories"][4]["category_name"]).to eq("sports")
+        expect(json["categories"][5]["category_name"]).to eq("technology")
       end
     end
   end
