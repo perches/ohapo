326bcfd5ecaea07807bbc3a092f9ae1c2f7ba9b1
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-09-23 16:28:47 +0900

    swagger修正

---
+++ api/src/db/migrate/20190923051902_user_news_categories.rb
@@ -0,0 +1,9 @@
+class UserNewsCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_news_categories do |t|
+      t.references :user, foreign_key: true, null: false
+      t.string :category, null: false
+      t.timestamps
+    end
+  end
+end
--- api/src/db/schema.rb
+++ api/src/db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_163657) do
+ActiveRecord::Schema.define(version: 2019_09_23_051902) do
+
+  create_table "user_news_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "category", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_news_categories_on_user_id"
+  end
+
+  create_table "user_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "age"
+    t.integer "gender", comment: "1:Male, 2:Female, 3:Others"
+    t.string "location"
+    t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -39,4 +58,6 @@ ActiveRecord::Schema.define(version: 2019_09_22_163657) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "user_news_categories", "users"
+  add_foreign_key "user_profiles", "users"
 end
